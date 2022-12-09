1273026e60d5aeacbe3c35e15c9db67ee5bf4c37
Calin1604 <120175978+Calin1604@users.noreply.github.com>
2022-12-09 14:12:49 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(300,200,20,20)
+  ball.shapeColor="red"
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown(LEFT_ARROW)){
+    ball.x-=2;
+  }
+  if(keyDown(RIGHT_ARROW)){
+    ball.x+=2;
+  }
+  if(keyDown(UP_ARROW)){
+    ball.y-=2;
+  }
+  if(keyDown(DOWN_ARROW)){
+    ball.y+=2;
+  }
+  drawSprites()
+
+}
+
+
+
+
